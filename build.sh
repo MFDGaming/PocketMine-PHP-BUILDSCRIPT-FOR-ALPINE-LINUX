408f23c46b175e8ea6f6fbe0266a1a62b092c086
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2020-06-06 10:59:36 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 apk update
 apk add make autoconf automake libtool m4 wget musl-utils gzip bzip2 bison g++ git cmake pkgconfig libressl-dev openssl libzip-dev curl-dev readline-dev yaml-dev libpng-dev gmp-dev bash --repository=http://dl-cdn.alpinelinux.org/alpine/edge/main
 wget https://www.php.net/distributions/php-7.4.6.tar.gz
+mv php-7.4.6 php-src
