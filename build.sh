d6ed40cf92d09a03c96aa290f61966dcea569ff1
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2020-06-06 11:03:57 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ apk add make autoconf automake libtool m4 wget musl-utils gzip bzip2 bison g++ g
 wget https://www.php.net/distributions/php-7.4.6.tar.gz
 mv php-7.4.6 php-src
 cd php-src
+./configure --prefix=/usr --with-config-file-path=/etc/php/7.2/cli --with-config-file-scan-dir=/etc/php/7.2/cli/conf.d --disable-all --with-openssl --with-gmp --with-zlib --with-curl --with-pic --enable-phar --enable-ctype --enable-sockets --enable-shared=no --enable-static=yes --enable-shmop --enable-maintainer-zts --enable-pcntl  --enable-mbstring --enable-calendar --with-tsrm-pthreads --enable-bcmath --enable-cli --enable-zip --enable-ftp --with-gd --enable-opcache=yes --enable-hash --enable-json
