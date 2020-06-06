ae53443f7bc7ca9efb821b4043ed1bbc81c98434
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2020-06-06 10:40:53 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
+apk update
+apk add make autoconf automake libtool m4 wget musl-utils gzip bzip2 bison g++ git cmake pkgconfig bash --repository=http://dl-cdn.alpinelinux.org/alpine/edge/main
 
-apk add make autoconf automake libtool m4 wget musl-utils gzip bzip2 bison g++ git cmake pkgconfig bash
+#!/bin/bash
