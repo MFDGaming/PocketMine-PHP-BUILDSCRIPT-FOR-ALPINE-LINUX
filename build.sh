8b3483d84e009d6b7ddfc92a3d07ca35b84a2573
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2020-06-06 10:38:37 +0300

    Update and rename phppmmp.sh to build.sh

---
+++ build.sh
@@ -0,0 +1,2 @@
+
+apk add make autoconf automake libtool m4 wget musl-utils gzip bzip2 bison g++ git cmake pkgconfig bash
--- phppmmp.sh
+++
@@ -1,1 +0,0 @@
-apk add g++ make autoconf automake libtool m4 wget musl-utils gzip bzip2 bash libxml2-dev icu-dev curl-dev pcre-dev
